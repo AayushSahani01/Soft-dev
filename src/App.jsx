4fa10372954e23cb1a4cb6ab2269d118c3438e4f
ranker-Aayush <anshusahani6104@gmail.com>
2025-09-22 23:11:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
             </>
           }
         /> */}
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
